e2ceb2f2bb0b28e77004600e4e9c1763425a9c71
D3x1a8 <D3x1a8@users.noreply.github.com>
2025-04-06 14:55:03 +0530

    Update [id].js

--- client/pages/api/products/related/[id].js
+++ client/pages/api/products/related/[id].js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   const { id } = req.query;
   try {
-    const apiResponse = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products/related/${id}`);
+    const apiResponse = await fetch(`http://localhost:3001/products/related/${id}`);
     const data = await apiResponse.json();
     res.status(200).json(data);
   } catch (error) {
